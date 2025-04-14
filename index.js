64875de1cfa78efc4e5233bc9b0746c8276431ef
Naominm <naomimbugua536@gmail.com>
2025-04-14 13:08:37 +0300

    Add deployed origin

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://blog-it-git-master-naomi-mbuguas-projects.vercel.app/login",
+    origin: "https://blog-it-git-master-naomi-mbuguas-projects.vercel.app",
     methods: ["POST", "GET", "PUT", "PATCH", "DELETE"],
     credentials: true,
   }),
