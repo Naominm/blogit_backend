fe66eff6e1412ba257e341b2aa186d0cd5935ab2
Naominm <naomimbugua536@gmail.com>
2025-04-14 07:32:07 +0300

    create controllers to refactor code in the index file

---
+++ controllers/authController.js
@@ -0,0 +1,66 @@
+import { PrismaClient } from "@prisma/client";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+const client = new PrismaClient();
+
+export const register = async (req, res) => {
+  const { firstName, lastName, emailAddress, userName, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 12);
+  try {
+    const newUser = await client.user.create({
+      data: {
+        firstName,
+        lastName,
+        emailAddress,
+        userName,
+        password: hashedPassword,
+      },
+    });
+    res.status(201).json({ message: "user created successfully" });
+  } catch (e) {
+    res.status(500).json({ message: "Something went wrong. please try again" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    //1. Read the identifier and the password from the client
+    const { identifier, password } = req.body;
+    //2. Get the record where emailAddress or username matches the identifier
+    const user = await client.user.findFirst({
+      where: {
+        OR: [{ emailAddress: identifier }, { userName: identifier }],
+      },
+    });
+
+    //3. If the record doesn't exist, wrong login
+    if (!user) {
+      return res.status(401).json({ message: "wrong login credentials" });
+    }
+    //4. if the record exist, compare the password with the stored hash password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    //5. If the password don't math, wrong login credentials.
+    if (!isMatch) {
+      return res.status(401).json({ message: "wrong login credentials" });
+    }
+    //6. if they match, save important info into a json web token and send the json web token to the client
+    const jwtPayload = {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    };
+    const token = jwt.sign(jwtPayload, process.env.JWT_SECRET_KEY);
+    res.status(200).cookie("blogitAuthToken", token, {}).json({
+      firstName: user.firstName,
+      lastName: user.lastName,
+      emailAddress: user.emailAddress,
+      userName: user.userName,
+    });
+  } catch (e) {
+    res.status(500).json({
+      message: "something went wrong",
+    });
+  }
+};
--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import express from "express";
-import { PrismaClient } from "@prisma/client";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcryptjs";
 import cors from "cors";
 import validateEmailAndUsername from "./middlewares/validateEmailAndUsername.js";
 import checkPasswordStrength from "./middlewares/checkPasswordStrength.js";
+import { register, login } from "./controllers/authController.js";
 
 const app = express();
 app.use(express.json());
@@ -16,74 +14,13 @@ app.use(
   }),
 );
 
-const client = new PrismaClient();
-
 app.post(
   "/auth/register",
   [validateEmailAndUsername, checkPasswordStrength],
-  async (req, res) => {
-    const { firstName, lastName, emailAddress, userName, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 12);
-    try {
-      const newUser = await client.user.create({
-        data: {
-          firstName,
-          lastName,
-          emailAddress,
-          userName,
-          password: hashedPassword,
-        },
-      });
-      res.status(201).json({ message: "user created successfully" });
-    } catch (e) {
-      res
-        .status(500)
-        .json({ message: "Something went wrong. please try again" });
-    }
-  },
+  register,
 );
 
-app.post("/auth/login", async (req, res) => {
-  try {
-    //1. Read the identifier and the password from the client
-    const { identifier, password } = req.body;
-    //2. Get the record where emailAddress or username matches the identifier
-    const user = await client.user.findFirst({
-      where: {
-        OR: [{ emailAddress: identifier }, { userName: identifier }],
-      },
-    });
-
-    //3. If the record doesn't exist, wrong login
-    if (!user) {
-      return res.status(401).json({ message: "wrong login credentials" });
-    }
-    //4. if the record exist, compare the password with the stored hash password
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    //5. If the password don't math, wrong login credentials.
-    if (!isMatch) {
-      return res.status(401).json({ message: "wrong login credentials" });
-    }
-    //6. if they match, save important info into a json web token and send the json web token to the client
-    const jwtPayload = {
-      id: user.id,
-      firstName: user.firstName,
-      lastName: user.lastName,
-    };
-    const token = jwt.sign(jwtPayload, process.env.JWT_SECRET_KEY);
-    res.status(200).cookie("blogitAuthToken", token, {}).json({
-      firstName: user.firstName,
-      lastName: user.lastName,
-      emailAddress: user.emailAddress,
-      userName: user.userName,
-    });
-  } catch (e) {
-    res.status(500).json({
-      message: "something went wrong",
-    });
-  }
-});
+app.post("/auth/login", login);
 
 const port = process.env.PORT || 4000;
 
